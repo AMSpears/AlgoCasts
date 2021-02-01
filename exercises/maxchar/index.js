c07363639c91bda9c08b6578c3e6072de6c4dada
Angie Spears <angie.spears809@gmail.com>
2021-02-01 15:39:51 -0500

    Complete maxChar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charList = {}
+  let maxVal = 0
+  let maxChar = ''
+
+  for (let char of str) {
+    if (!charList[char]) {
+      charList[char] = 1
+    } else {
+      charList[char]++
+    }
+  }
+
+  for (let char in charList) {
+    let val = charList[char]
+    if (val > maxVal) {
+      maxVal = val
+      maxChar = char
+    }
+  }
+
+  return maxChar
+}
 
 module.exports = maxChar;
